c643e99dbfa84219997cf6ab3b1971c784270c58
JuYoung <ken44929@gmail.com>
2021-08-18 18:39:26 +0900

    Feat: Add routes' path

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import AdminPage from './pages/AdminPage/AdminPage';
 import PaginationPage from './pages/PaginationPage/PaginationPage';
+import RegisterPage from './pages/RegisterPage/RegisterPage';
+import { ROUTES } from './utils/routes';
 
 const App = () => {
   return (
     <div>
-      <Route exact path="/" component={AdminPage}></Route>
-      <Route path="/pagination" component={PaginationPage}></Route>
+      <Route exact path={ROUTES.MAIN} component={AdminPage} />
+      <Route path={ROUTES.PAGINATION} component={PaginationPage} />
+      <Route path={ROUTES.REGISTER} component={RegisterPage} />
     </div>
   );
 };
